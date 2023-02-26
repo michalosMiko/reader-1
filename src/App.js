03b823e7f59391dcebe1d5bdc82c02a933c73f76
MichalMamula <michalosm@centrum.cz>
2023-02-26 10:10:44 +0100

    TextToSpeak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import TextToSpeech from "./components/TextToSpeach"
+import "./index.css"
 
-function App() {
+const App = () => {
+  const text = 'Vítáme vás na palubě lodi M-web-x ';
+  // ťext který se přečte ale nezobrazí
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Čtečka skrytého textu</h1>
+      <TextToSpeech text={text}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
